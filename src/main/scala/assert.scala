7900b9dfc56b2d093205000909f65453f522d0f4
testg1tuser <jangililavanya@gmail.com>
2021-07-09 18:12:12 +0530

    Revert "added new line for branch testing"
    This reverts commit 49567c56aebe61d129238502454a048b456de194.

--- src/main/scala/assert.scala
+++ src/main/scala/assert.scala
@@ -2,4 +2,3 @@ val ns = List(1, 2, 3, 4)
 val s0 = ns.foldLeft (0) (_+_) //10
 val s1 = ns.fold (1) (_+_) //10
 assert(s0 == s1) //AssertionError: assertion failed
-//Adding new line for branching testing
